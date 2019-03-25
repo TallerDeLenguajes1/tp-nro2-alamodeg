2a16a3dfaf0f67820152dc84073aa35d17b37436
alamodeg <alvarodeguer@gmail.com>
2019-03-24 23:49:54 -0300

    Ordenando1

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -3,20 +3,15 @@
 #define N 4
 #define M 5
 
-
 int main(){
 	int f,c;
-	double mt[N][M];
+	double matriz[N][M];
 
-
-	for(f = 0;f<N; f++)
-	{
- 	for(f = 0;f<N; f++)
-	{
-	printf("%lf ", mt[f][c]);
+	for(f = 0;f<N; f++){
+		for(c = 0;c<N; c++){
+			printf("%lf ", matriz[N][M]);
+		}
+		printf("\n");
 	}
-	printf("\n");
-	}
-
 	return 0;
 }
