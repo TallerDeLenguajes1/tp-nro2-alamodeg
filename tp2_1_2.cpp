5862ed61cc5bd17bd36f73ad992ea99b6e0c63c9
Alvaro D <alvarodeguer@gmail.com>
2019-03-27 20:07:31 -0300

    Ordenando el lio

--- tp2_1_2.cpp
+++ tp2_1_2.cpp
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #define N 4
 #define M 5
-// OPCION NUMERO 1, BRANCH MASTER//
+// OPCION NUMERO 2, BRANCH OPCION_2//
 int main(){
-	int f,c;
+	int f,c,i;
 	double matriz[N][M];
+	double *puntero = &matriz[0][0];
 
 	for(f = 0;f<N; f++){
 		for(c = 0;c<N; c++){
-			printf("%lf ", matriz[N][M]);
+			*puntero=rand()%10;
+			printf(" %.2lf ", *(puntero++));
 		}
 		printf("\n");
 	}
