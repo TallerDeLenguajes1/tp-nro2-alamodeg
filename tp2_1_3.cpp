7e684ae31fe1319c793e257ea163058acbdd606a
alamodeg <alvarodeguer@gmail.com>
2019-03-30 21:05:42 -0300

    Corrigiendo errores

--- tp2_1_3.cpp
+++ tp2_1_3.cpp
@@ -7,39 +7,43 @@
 int main(){
 	srand(time(NULL));
 	int *matriz;
-	int columnas = 5 + rand() % ((15+1)-5);
+	int COLUMNAS = 5 + rand() % ((15+1)-5);
 	int cont_pares = 0;
-	int pares[FILAS];
+	int *punt_pares;
 
 	matriz = (int*)malloc(FILAS*sizeof(int));
-	for (int i = 0; i < FILAS*columnas; i++){ //Relleno la matriz con filas*columnas
+	punt_pares = (int*)malloc(FILAS*sizeof(int));
+	//--------------CARGA DE MATRIZ
+	for (int i = 0; i < FILAS*COLUMNAS; i++){ //Relleno la matriz con filas*COLUMNAS
 		int aleatorio = 100 + rand() % ((999+1)-100);   //Menor + rand()%(Mayor+1)-Menor);
 		*(matriz+i) = aleatorio;
 	}
-	puts("|-----------MATRIZ CARGADA-----------|\n");
+	/*	puts("|-----------MUESTRA MATRIZ-----------|\n");
 	for(int i=0;i<FILAS;i++){ 
-		for(int j=0;j<columnas;j++){
-			printf("%d ",*(matriz+((i*columnas)+j)));
+		for(int j=0;j<COLUMNAS;j++){
+			//printf("%d ",*(matriz+((i*COLUMNAS)+j)));
 		}
 		printf("\n");
-	}
-	printf("\n\n");
-	puts("|-----------CANTIDAD DE PARES-----------|");
+	}	*/
+	puts("|-----------MATRIZ CARGADA CON PARES-----------|");
 	for(int i=0;i<FILAS;i++){ 
-		for(int j=0;j<columnas;j++){
-			if ( *(matriz+((i*columnas)+j))%2==0){
+		for(int j=0;j<COLUMNAS;j++){
+			printf("%d ",*(matriz+((i*COLUMNAS)+j)));
+			if ( *(matriz+((i*COLUMNAS)+j))%2==0){
 				cont_pares++;
-
 			}
 		}
-		pares[i] = cont_pares;
-		printf("El numero de pares de la fila N%d es %d\n",i+1,cont_pares);
+		*(punt_pares+i) = cont_pares;
+		printf("        Pares: %d\n",cont_pares);
 		cont_pares=0;
 	}
 	printf("\n\n");
 	puts("|-----------VECTOR CON CANTIDAD DE PARES POR FILA-----------|");
 	for (int i = 0; i < FILAS; i++){
-		printf("%d  ",pares[i]);
+		printf("%d - ",*(punt_pares+i));
 	}
+	free(matriz);
+	free(punt_pares);
+
 	return 0;
 } 
